401b09f9886380a61d92bc5f6f994d171924cc75
Calum <cramsel2319@gmail.com>
2023-01-05 13:58:40 -0600

    Box changes color to green successfully

--- HFCSColorChangeEx/Form1.Designer.cs
+++ HFCSColorChangeEx/Form1.Designer.cs
@@ -28,20 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Change the color!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(285, 38);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Color Check";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(86, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Watch the color change!";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(449, 184);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- HFCSColorChangeEx/Form1.cs
+++ HFCSColorChangeEx/Form1.cs
@@ -12,9 +12,29 @@ namespace HFCSColorChangeEx
 {
     public partial class Form1 : Form
     {
+        int checkBox = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox == 0)
+            {
+                MessageBox.Show("Box is not checked.");
+            } else
+            {
+                label1.BackColor = Color.Green;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox = 1;
+            }
         }
     }
 }
