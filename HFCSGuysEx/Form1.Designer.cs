4ee67a14c35b215f1f8ba62b83d5483f08733f78
Calum <cramsel2319@gmail.com>
2023-01-09 13:53:23 -0600

    Base properties for form configured, committed as hitting ctrl+z when you accidentally double click a label tends to wipe out a LOT more than just the new on click method

--- HFCSGuysEx/Form1.Designer.cs
+++ HFCSGuysEx/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.joeNameLabel = new System.Windows.Forms.Label();
+            this.bobNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // joeNameLabel
+            // 
+            this.joeNameLabel.AutoSize = true;
+            this.joeNameLabel.Location = new System.Drawing.Point(12, 9);
+            this.joeNameLabel.Name = "joeNameLabel";
+            this.joeNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.joeNameLabel.TabIndex = 0;
+            this.joeNameLabel.Text = "joeNameLabel";
+            // 
+            // bobNameLabel
+            // 
+            this.bobNameLabel.AutoSize = true;
+            this.bobNameLabel.Location = new System.Drawing.Point(15, 58);
+            this.bobNameLabel.Name = "bobNameLabel";
+            this.bobNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobNameLabel.TabIndex = 1;
+            this.bobNameLabel.Text = "bobNameLabel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(347, 239);
+            this.Controls.Add(this.bobNameLabel);
+            this.Controls.Add(this.joeNameLabel);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label joeNameLabel;
+        private System.Windows.Forms.Label bobNameLabel;
     }
 }
 
---
+++ HFCSGuysEx/Guy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HFCSGuysEx
+{
+    internal class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            } else
+            {
+                MessageBox.Show("I don't have enough for that.");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            } else
+            {
+                MessageBox.Show(amount + " isn't an amount I'll take, " + Name + " says.");
+                return 0;
+            }
+        }
+    }
+}
