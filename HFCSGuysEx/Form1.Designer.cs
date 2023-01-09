c9c627c3f65f7d719e98e7c009e470f10c724ea7
Calum <cramsel2319@gmail.com>
2023-01-09 14:34:34 -0600

    HFCSGuysEx Finished, app properly adds and subtracts

--- HFCSGuysEx/Form1.Designer.cs
+++ HFCSGuysEx/Form1.Designer.cs
@@ -30,31 +30,63 @@
         {
             this.joeNameLabel = new System.Windows.Forms.Label();
             this.bobNameLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.give10Button = new System.Windows.Forms.Button();
+            this.receive5Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // joeNameLabel
             // 
             this.joeNameLabel.AutoSize = true;
-            this.joeNameLabel.Location = new System.Drawing.Point(12, 9);
+            this.joeNameLabel.Location = new System.Drawing.Point(9, 9);
             this.joeNameLabel.Name = "joeNameLabel";
-            this.joeNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.joeNameLabel.Size = new System.Drawing.Size(0, 13);
             this.joeNameLabel.TabIndex = 0;
-            this.joeNameLabel.Text = "joeNameLabel";
             // 
             // bobNameLabel
             // 
             this.bobNameLabel.AutoSize = true;
-            this.bobNameLabel.Location = new System.Drawing.Point(15, 58);
+            this.bobNameLabel.Location = new System.Drawing.Point(9, 58);
             this.bobNameLabel.Name = "bobNameLabel";
-            this.bobNameLabel.Size = new System.Drawing.Size(35, 13);
+            this.bobNameLabel.Size = new System.Drawing.Size(0, 13);
             this.bobNameLabel.TabIndex = 1;
-            this.bobNameLabel.Text = "bobNameLabel";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(12, 109);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(0, 13);
+            this.bankCashLabel.TabIndex = 2;
+            // 
+            // give10Button
+            // 
+            this.give10Button.Location = new System.Drawing.Point(12, 169);
+            this.give10Button.Name = "give10Button";
+            this.give10Button.Size = new System.Drawing.Size(148, 58);
+            this.give10Button.TabIndex = 3;
+            this.give10Button.Text = "Give $10 to Joe";
+            this.give10Button.UseVisualStyleBackColor = true;
+            this.give10Button.Click += new System.EventHandler(this.give10Button_Click);
+            // 
+            // receive5Button
+            // 
+            this.receive5Button.Location = new System.Drawing.Point(187, 169);
+            this.receive5Button.Name = "receive5Button";
+            this.receive5Button.Size = new System.Drawing.Size(148, 58);
+            this.receive5Button.TabIndex = 4;
+            this.receive5Button.Text = "Receive $5 from Bob";
+            this.receive5Button.UseVisualStyleBackColor = true;
+            this.receive5Button.Click += new System.EventHandler(this.receive5Button_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(347, 239);
+            this.Controls.Add(this.receive5Button);
+            this.Controls.Add(this.give10Button);
+            this.Controls.Add(this.bankCashLabel);
             this.Controls.Add(this.bobNameLabel);
             this.Controls.Add(this.joeNameLabel);
             this.MaximizeBox = false;
@@ -70,6 +102,9 @@
 
         private System.Windows.Forms.Label joeNameLabel;
         private System.Windows.Forms.Label bobNameLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button give10Button;
+        private System.Windows.Forms.Button receive5Button;
     }
 }
 
--- HFCSGuysEx/Form1.cs
+++ HFCSGuysEx/Form1.cs
@@ -12,9 +12,43 @@ namespace HFCSGuysEx
 {
     public partial class Form1 : Form
     {
+        Guy joe;
+        Guy bob;
+        int bank = 100;
+
         public Form1()
         {
             InitializeComponent();
+            joe = new Guy();
+            joe.Name = "Joe";
+            joe.Cash = 50;
+            bob = new Guy();
+            bob.Name = "Bob";
+            bob.Cash = 50;
+
+            UpdateForm();
+        }
+
+        public void UpdateForm()
+        {
+            joeNameLabel.Text = joe.Name + " has $" + joe.Cash;
+            bobNameLabel.Text = bob.Name + " has $" + bob.Cash;
+            bankCashLabel.Text = "The bank has $" + bank;
+        }
+
+        private void give10Button_Click(object sender, EventArgs e)
+        {
+            if (bank >= 10)
+            {
+                bank -= joe.ReceiveCash(10);
+                UpdateForm();
+            }
+        }
+
+        private void receive5Button_Click(object sender, EventArgs e)
+        {
+            bank += bob.GiveCash(5);
+            UpdateForm();
         }
     }
 }
