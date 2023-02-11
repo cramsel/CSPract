974de9ff5d39452180128a818c6d39efeee2a1cf
Calum <cramsel2319@gmail.com>
2023-02-10 19:46:10 -0600

    Design commit for HFCSElephantEx

--- HFCSElephantEx/Form1.Designer.cs
+++ HFCSElephantEx/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LloydButton = new System.Windows.Forms.Button();
+            this.LuciaButton = new System.Windows.Forms.Button();
+            this.SwapButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LloydButton
+            // 
+            this.LloydButton.Location = new System.Drawing.Point(12, 12);
+            this.LloydButton.Name = "LloydButton";
+            this.LloydButton.Size = new System.Drawing.Size(125, 31);
+            this.LloydButton.TabIndex = 0;
+            this.LloydButton.Text = "Lloyd";
+            this.LloydButton.UseVisualStyleBackColor = true;
+            // 
+            // LuciaButton
+            // 
+            this.LuciaButton.Location = new System.Drawing.Point(12, 49);
+            this.LuciaButton.Name = "LuciaButton";
+            this.LuciaButton.Size = new System.Drawing.Size(125, 32);
+            this.LuciaButton.TabIndex = 1;
+            this.LuciaButton.Text = "Lucia";
+            this.LuciaButton.UseVisualStyleBackColor = true;
+            // 
+            // SwapButton
+            // 
+            this.SwapButton.Location = new System.Drawing.Point(12, 87);
+            this.SwapButton.Name = "SwapButton";
+            this.SwapButton.Size = new System.Drawing.Size(125, 30);
+            this.SwapButton.TabIndex = 2;
+            this.SwapButton.Text = "Swap";
+            this.SwapButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(152, 146);
+            this.Controls.Add(this.SwapButton);
+            this.Controls.Add(this.LuciaButton);
+            this.Controls.Add(this.LloydButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button LloydButton;
+        private System.Windows.Forms.Button LuciaButton;
+        private System.Windows.Forms.Button SwapButton;
     }
 }
 
