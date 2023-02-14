5d290755910a7ec4eee0d34fd1350ac745f70417
Calum <cramsel2319@gmail.com>
2023-02-14 13:41:44 -0600

    Add form elements. Commit is just in case something goes horribly wrong, as I do not wish to drag and drop these again

--- HFCSMenuMakerEx/MenuMaker.cs
+++ HFCSMenuMakerEx/MenuMaker.cs
@@ -13,5 +13,14 @@ namespace HFCSMenuMakerEx
         string[] Meats = { "Roast Beef", "Salami", "Turkey", "Ham", "Pastrami"};
         string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing"};
         string[] Breads = { "rye", "white", "wheat", "pumpernickel", "italian bread", "a roll"};
+
+        public string GetMenuItem()
+        {
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomDressing = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+
+            return randomMeat + " with " + randomDressing + " on " + randomBread;
+        }
     }
 }
