85dc338182676ba995f0d95c8d4bea9c3a8c891f
Calum <cramsel2319@gmail.com>
2023-02-15 15:22:11 -0600

    Final commit for HFCSTypingGameEx, game   r runs as intended

--- HFCSTypingGameEx/Form1.Designer.cs
+++ HFCSTypingGameEx/Form1.Designer.cs
@@ -29,12 +29,122 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.CorrectLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.MissedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.TotalLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.AccuracyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.DifficultyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.DifficultyProgBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 108;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1046, 127);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CorrectLabel,
+            this.MissedLabel,
+            this.TotalLabel,
+            this.AccuracyLabel,
+            this.DifficultyLabel,
+            this.DifficultyProgBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 105);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1046, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // CorrectLabel
+            // 
+            this.CorrectLabel.Name = "CorrectLabel";
+            this.CorrectLabel.Size = new System.Drawing.Size(52, 17);
+            this.CorrectLabel.Text = "Correct: ";
+            // 
+            // MissedLabel
+            // 
+            this.MissedLabel.Name = "MissedLabel";
+            this.MissedLabel.Size = new System.Drawing.Size(50, 17);
+            this.MissedLabel.Text = "Missed: ";
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(38, 17);
+            this.TotalLabel.Text = "Total: ";
+            // 
+            // AccuracyLabel
+            // 
+            this.AccuracyLabel.Name = "AccuracyLabel";
+            this.AccuracyLabel.Size = new System.Drawing.Size(62, 17);
+            this.AccuracyLabel.Text = "Accuracy: ";
+            // 
+            // DifficultyLabel
+            // 
+            this.DifficultyLabel.Name = "DifficultyLabel";
+            this.DifficultyLabel.Size = new System.Drawing.Size(727, 17);
+            this.DifficultyLabel.Spring = true;
+            this.DifficultyLabel.Text = "Difficulty";
+            this.DifficultyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // DifficultyProgBar
+            // 
+            this.DifficultyProgBar.Maximum = 701;
+            this.DifficultyProgBar.Name = "DifficultyProgBar";
+            this.DifficultyProgBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1046, 127);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel CorrectLabel;
+        private System.Windows.Forms.ToolStripStatusLabel MissedLabel;
+        private System.Windows.Forms.ToolStripStatusLabel TotalLabel;
+        private System.Windows.Forms.ToolStripStatusLabel AccuracyLabel;
+        private System.Windows.Forms.ToolStripStatusLabel DifficultyLabel;
+        private System.Windows.Forms.ToolStripProgressBar DifficultyProgBar;
     }
 }
 
--- HFCSTypingGameEx/Form1.cs
+++ HFCSTypingGameEx/Form1.cs
@@ -12,9 +12,53 @@ namespace HFCSTypingGameEx
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+        Stats stats = new Stats();
+
         public Form1()
         {
             InitializeComponent();
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("GAME OVER");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                if (timer1.Interval > 100)
+                    timer1.Interval -= 2;
+
+                DifficultyProgBar.Value = 800 - timer1.Interval;
+
+                stats.Update(true);
+            } else
+            {
+                stats.Update(false);
+            }
+
+            CorrectLabel.Text = "Correct: " + stats.Correct;
+            MissedLabel.Text = "Missed: " + stats.Missed;
+            TotalLabel.Text = "Total: " + stats.Total;
+            AccuracyLabel.Text = "Accuracy: " + stats.Accuracy + "%";
         }
     }
 }
